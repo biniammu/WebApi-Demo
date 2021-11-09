cc1c368f12d5199c6fdc0713a10fb96c0a4ff825
biniammu <91587425+biniammu@users.noreply.github.com>
2021-11-09 15:45:41 +0530

    Add project files.

---
+++ WebApiDemo/Controllers/StateController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class StateController : ApiController
+    {
+        StateRepository stateRepository = new StateRepository();
+
+        // GET: api/State
+        public IEnumerable<States> Get()
+        {
+            return stateRepository.GetStates();
+        }
+
+        //// GET: api/State/5
+        public States Get(int id)
+        {
+            return stateRepository.GetStateById(id);
+        }
+
+        // POST: api/State
+        public void Post([FromBody]States state)
+        {
+            stateRepository.InsertState(state);
+        }
+
+        // PUT: api/State/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/State/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiDemo/StateRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApiDemo.Models;
+
+namespace WebApiDemo
+{
+    public class StateRepository
+    {
+        SqlConnection _sqlConnection = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=TrainingManagement;Integrated Security=True");
+
+        public IEnumerable<States> GetStates()
+        {
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter("spGetStates", _sqlConnection);
+            da.Fill(ds);
+            List<States> states = new List<States>();
+            foreach (DataRow item in ds.Tables[0].Rows)
+            {
+                States state = new States
+                {
+                    StateId = Int32.Parse(item["StateId"].ToString()),
+                    StateName = item["StateName"].ToString(),
+                };
+                states.Add(state);
+            }
+            return states;
+        }
+
+        public States GetStateById(int id)
+        {
+            DataSet ds = new DataSet();
+            SqlCommand sqlCommand = new SqlCommand("spGetStateById", _sqlConnection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@pStateId", id);
+            SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+            da.Fill(ds, "States");
+
+            DataRow row = ds.Tables["States"].Rows[0];
+            States state = new States
+            {
+                StateId = Int32.Parse(row["StateId"].ToString()),
+
+                StateName = row["StateName"].ToString()
+            };
+
+            return state;
+        }
+        public void InsertState(States state)
+        {
+            _sqlConnection.Open();
+            SqlCommand command = new SqlCommand("spInsertState", _sqlConnection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@pStateName", state.StateName);
+            command.ExecuteNonQuery();
+            command.Dispose();
+            _sqlConnection.Close();
+        }
+    }
+}
